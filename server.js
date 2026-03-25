e50aa1a420d3026eaa355a523985c4b41bf7d3fd
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-03-25 11:21:04 +0530

    - Created a "tourSchema". - Created a "Tour" model.

--- server.js
+++ server.js
@@ -13,7 +13,25 @@ mongoose.connect("mongodb://localhost:27017/natours", {
         console.log("Connection error ❌:", err);
     });
 
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "A tour must have a name"],
+        unique: true,
+    },
+    rating: {
+        type: Number,
+        default: 4.5,
+    },
+    price: {
+        type: Number,
+        required: [true, "A tour must have a price"],
+    },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
+
 const port = 3000;
 app.listen(port, () => {
-  console.log(`App running on port ${port}.....`);
+    console.log(`App running on port ${port}.....`);
 });
