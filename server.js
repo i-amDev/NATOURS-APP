6dca7e028f4cec61b9e59a6ae91ae5f449e8dd68
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-03-25 11:10:06 +0530

    - Connected my Node.js application with "MongoDB".

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/natours", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+    .then(() => {
+        console.log("Connected to MongoDB ✅");
+    })
+    .catch((err) => {
+        console.log("Connection error ❌:", err);
+    });
 
 const port = 3000;
 app.listen(port, () => {
