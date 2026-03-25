ad2e21afbdfdf15330f8ff5f1cff878dc9054a44
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-03-25 22:48:02 +0530

    - Learned more about aggregation pipelines.

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -8,6 +8,8 @@ router.route("/top-5-cheap").get(tourController.aliasTopTours, tourController.ge
 
 router.route("/tour-stats").get(tourController.getTourStats);
 
+router.route("/monthly-plan/:year").get(tourController.getMonthlyPlan);
+
 router.get("/", tourController.getAllTours);
 
 router.post("/", tourController.createTour);
