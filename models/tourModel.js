9dbbe4eec1d9aff5c0a1481ad05d24710d182ca9
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-03-26 13:32:00 +0530

    - Learned about custom validators.

--- models/tourModel.js
+++ models/tourModel.js
@@ -41,7 +41,15 @@ const tourSchema = new mongoose.Schema({
         type: Number,
         required: [true, "A tour must have a price"],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+        type: Number,
+        validate: {
+            validator: function(value) {
+                return value < this.price;
+            },
+            message: "Discount price ({VALUE}) should be below regular price."
+        }
+    },
     summary: {
         type: String,
         trim: true,
