2ced25d1c8f1d6f50dc5be7f9eb968fe6ee210fb
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-03-26 12:46:04 +0530

    - Learned about "Aggregation middleware" in mongoose. - We can execute this before any aggregation pipeline.

--- models/tourModel.js
+++ models/tourModel.js
@@ -97,9 +97,15 @@ tourSchema.pre(/^find/, function (next) {
 
 tourSchema.post(/^find/, function (document, next) {
     console.log(`Query took ${Date.now() - this.start}ms`);
-    console.log(document);
     next();
-})
+});
+
+// Aggregation Middleware
+tourSchema.pre("aggregate", function (next) {
+    this.pipeline().unshift({$match: {secretTour: {$ne: true}}});
+    console.log(this.pipeline());
+    next();
+});
 
 const Tour = mongoose.model("Tour", tourSchema);
 
