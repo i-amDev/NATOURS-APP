c0c52d65467966a7b58a70e90fd1d2400673d4f6
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-03-26 13:10:41 +0530

    - Learned about validators in Node.js.

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,7 +6,9 @@ const tourSchema = new mongoose.Schema({
         type: String,
         required: [true, "A tour must have a name"],
         unique: true,
-        trim: true
+        trim: true,
+        maxlength: [40, "A tour name must have less or equal then 40 characters"],
+        minLength: [10, "A tour name must have more or equal then 10 characters"]
     },
     slug: String,
     duration: {
@@ -20,10 +22,16 @@ const tourSchema = new mongoose.Schema({
     difficulty: {
         type: String,
         required: [true, "A tour must have a difficulty"],
+        enum: {
+            values : ["easy", "medium", "difficult"],
+            message: "Difficulty is either easy, medium or difficult"
+        }
     },
     ratingsAverage: {
         type: Number,
         default: 4.5,
+        min: [1, "Rating must be above 1.0"],
+        max: [5, "Rating must be below 5.0"]
     },
     ratingsQuantity: {
         type: Number,
