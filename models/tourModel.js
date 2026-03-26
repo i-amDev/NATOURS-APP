7e11a1e1821f8e4f4ff4b78ca0bed98741477238
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-03-26 12:24:33 +0530

    - Learned about "Document middleware" in mongoose. - It runs before the save and create command.

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema({
         unique: true,
         trim: true
     },
+    slug: String,
     duration: {
         type: Number,
         required: [true, "A tour must have a duration"],
@@ -59,6 +61,22 @@ const tourSchema = new mongoose.Schema({
 
 tourSchema.virtual("durationWeeks").get(function () {
     return this.duration / 7;
+});
+
+// Document Middleware -> Runs before the save and create command
+tourSchema.pre("save", function (next) {
+    this.slug = slugify(this.name, {lower: true});
+    next();
+});
+
+tourSchema.pre("save", function (next) {
+    console.log("Will save document....");
+    next();
+});
+
+tourSchema.post("save", function (document, next) {
+    console.log(document);
+    next();
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
