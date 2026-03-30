973771fb5f5733485a2149e1557af60432bc4b89
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-03-30 12:08:34 +0530

    - Added the logic to create the "json web Token".

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
+const jwt = require("jsonwebtoken");
 
 exports.signup = catchAsync(async (req, res, next) => {
     const newUser = await User.create({
@@ -9,8 +10,15 @@ exports.signup = catchAsync(async (req, res, next) => {
         passwordConfirm: req.body.passwordConfirm,
     });
 
+    const JWT_SECRET = "my-ultra-secure-and-ultra-long-secret";
+
+    const JWT_EXPIRATION = 60 * 60 * 1000;
+
+    const token = jwt.sign({id : newUser._id}, JWT_SECRET, {expiresIn: JWT_EXPIRATION});
+
     res.status(201).json({
         status: "success",
+        token,
         data : {
             user: newUser,
         }
