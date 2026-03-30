2378e329ee66190d905bf68ec9b27a005abbf31f
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-03-30 20:19:28 +0530

    - Added the custom "TokenExpiredError".

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,6 +1,13 @@
+const AppError = require("../utils/appError");
+
+const handleJWTError = () =>
+    new AppError('Invalid token. Please log in again!', 401);
+
 module.exports = (error, request, response, next) => {
     error.statusCode = error.statusCode || 500;
     error.status = error.status || "Error";
+
+    if (error.name === 'TokenExpiredError') error = handleJWTError();
 
     response.status(error.statusCode).json({
         status: error.status,
