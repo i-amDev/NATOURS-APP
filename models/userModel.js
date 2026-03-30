df3a887301e9440e3578f91241465ba99420c61b
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-03-30 20:18:26 +0530

    - Added a new field "passwordChangedAt" in the userSchema.

--- models/userModel.js
+++ models/userModel.js
@@ -31,7 +31,8 @@ const userSchema = new mongoose.Schema({
             },
             message: "Passwords are not the same"
         }
-    }
+    },
+    passwordChangedAt: Date
 });
 
 userSchema.pre("save", async function (next) {
@@ -47,6 +48,15 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
     return await bcrypt.compare(candidatePassword, userPassword);
+}
+
+userSchema.methods.changedPasswordAfter = function (JWTTimeStamp) {
+    if (this.passwordChangedAt) {
+        const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
+        // console.log(changedTimestamp, JWTTimeStamp);
+        return JWTTimeStamp < changedTimestamp;
+    }
+    return false;
 }
 
 const User = mongoose.model("User", userSchema);
