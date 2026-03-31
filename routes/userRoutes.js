b305615a29d5a6d0b3a4f724b0da66b1acc0b6e9
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-03-31 13:51:11 +0530

    - Added two routes "/forgotPassword" and "/resetPassword".

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,6 +9,10 @@ router.post("/signup", authController.signup);
 
 router.post("/login", authController.login);
 
+router.post("/forgotPassword", authController.forgotPassword);
+
+router.post("/resetPassword", authController.resetPassword);
+
 router
   .route("/")
   .get(userController.getAllUsers)
