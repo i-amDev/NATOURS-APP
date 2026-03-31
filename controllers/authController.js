41dea1d4351d837c0b3bafb5cf31cd722d2d3a51
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-03-31 15:48:18 +0530

    - Implemented the complete logic of "resetPassword" method.

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,8 @@ const catchAsync = require("./../utils/catchAsync");
 const jwt = require("jsonwebtoken");
 const AppError = require("./../utils/appError");
 const {promisify} = require("util");
-const sendEmail = require("./../utils/email")
+const sendEmail = require("./../utils/email");
+const crypto = require("crypto");
 
 const JWT_SECRET = "my-ultra-secure-and-ultra-long-secret";
 
@@ -131,4 +132,28 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 });
 
 exports.resetPassword = catchAsync(async (req, res, next) => {
+    // Get user based on the token
+    const hashedToken = crypto.createHash("sha256").update(req.params.token).digest("hex");
+
+    const user = await User.findOne({passwordResetToken: hashedToken, passwordResetExpires: {$gt: Date.now()}});
+
+    // If token has not expired, and there is user, set the new password
+    if (!user) {
+        return next(new AppError("Token is invalid or has expired", 400));
+    }
+    user.password = req.body.password;
+    user.passwordConfirm = req.body.passwordConfirm;
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save();
+
+    // Update changedPasswordAt property for the user
+    // Done this in the "userModel"
+
+    // Log the user in, send JWT
+    const token = signToken(user._id);
+    res.status(200).json({
+        status: "success",
+        token
+    });
 })
