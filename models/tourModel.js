b2257f873c239b5dc0e9694a4ed53126164854f6
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-04-01 12:24:41 +0530

    - Added "guides" field in the tourSchema. - Implemented one document middleware on "save" to fetch all the users as well in the "getAllTours" endpoint.

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const User = require("./userModel");
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -96,7 +97,8 @@ const tourSchema = new mongoose.Schema({
             description: String,
             date: Number
         }
-    ]
+    ],
+    guides: Array
 }, {
     toJSON: {virtuals: true},
     toObject: {virtuals: true},
@@ -109,6 +111,12 @@ tourSchema.virtual("durationWeeks").get(function () {
 // Document Middleware -> Runs before the save and create command
 tourSchema.pre("save", function (next) {
     this.slug = slugify(this.name, {lower: true});
+    next();
+});
+
+tourSchema.pre("save", async function (next) {
+    const guidesPromises = this.guides.map(async id => await User.findById(id));
+    this.guides = await Promise.all(guidesPromises);
     next();
 });
 
