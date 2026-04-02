910dafc0bed512208a59b393e1649d7561f756c7
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-04-02 14:34:03 +0530

    - Added the "getAll" "Generic" method in "handlerFactory".

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,5 +1,6 @@
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
+const APIFeatures = require("./../utils/apiFeatures");
 
 exports.deleteOne = Model => catchAsync(async (request, response, next) => {
     const doc = await Model.findByIdAndDelete(request.params.id);
@@ -56,6 +57,25 @@ exports.getOne = (Model, popOptions) => catchAsync(async (request, response, nex
 
     response.status(200).json({
         status: "success",
+        data: {
+            doc,
+        },
+    });
+});
+
+exports.getAll = Model => catchAsync(async (request, response) => {
+    // To allow for nested GET reviews on tour
+    let filter = {};
+    if (request.params.tourId) {
+        filter = {tour: request.params.tourId};
+    }
+
+    const features = new APIFeatures(Model.find(filter), request.query).filter().sort().limitFields().paginate();
+    const doc = await features.query;
+
+    response.status(200).json({
+        status: "success",
+        results: doc.length,
         data: {
             doc,
         },
