1dc0e49637fc2dd2e6437c345ae7bc27ba4d5344
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-04-02 12:02:06 +0530

    - Created "reviewRoutes" for routing review endpoints.

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const globalErrorHandler = require("./controllers/errorController");
 
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
+const reviewRouter = require("./routes/reviewRoutes");
 
 const app = express();
 
@@ -58,6 +59,7 @@ app.use((request, response, next) => {
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/reviews", reviewRouter);
 
 app.use((request, response, next) => {
     // response.status(404).json({
